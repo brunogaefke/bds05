2f2e69515f3a3ed9b6929de76e2457ddeed240d6
Bruno Gaefke <brunogaefke@gmail.com>
2021-10-20 16:01:35 -0300

    fix end point and tests ok

--- src/main/java/com/devsuperior/movieflix/dto/UserDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/UserDTO.java
@@ -10,19 +10,22 @@ public class UserDTO implements Serializable {
 	private Long id;
 	private String name;
 	private String email;
+	private Long userId;
 	
 	public UserDTO() {}
 
-	public UserDTO(Long id, String name, String email) {
+	public UserDTO(Long id, String name, String email, Long userId) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
+		this.userId = userId;
 	}
 	
 	public UserDTO(User entity) {
 		id = entity.getId();
 		name = entity.getName();
 		email = entity.getEmail();
+		userId = entity.getId();
 	}
 
 	public Long getId() {
@@ -47,5 +50,13 @@ public class UserDTO implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
 	}
 }
--- src/main/java/com/devsuperior/movieflix/resourcess/UserResource.java
+++ src/main/java/com/devsuperior/movieflix/resourcess/UserResource.java
@@ -3,7 +3,6 @@ package com.devsuperior.movieflix.resourcess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +12,12 @@ import com.devsuperior.movieflix.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping(value = "/profile")
-	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
-		UserDTO dto = service.findById(id);
-		return ResponseEntity.ok().body(dto);
+	public ResponseEntity<UserDTO> getProfile() {
+		UserDTO dto = service.getProfile();
+		return ResponseEntity.ok(dto);
 	}
 }
